a2d0f800728a8a81c4dc86e95bcaabaa792e3111
SafteyDav3 <talktomedw@gmail.com>
2022-03-11 22:42:00 -0600

    Added all selector and body/font-family.

---
+++ docs/j-script/foo.js
@@ -0,0 +1,22 @@
+// ↑↑↑ This is the top of the page. ↑↑↑
+var car = {
+  make: "ford",
+  model: "probe",
+  color: ["blue", "Cream", "Alloy"],
+  year: 1998,
+  rusted: true,
+  plow: true,
+  honk: function () {
+    console.log("Get outta my way!");
+  },
+
+  motor: {
+    liters: 4.3,
+    horsePower: 3,
+    turbo: true,
+    superCharged: false,
+    pistons: 12,
+    tech: "Vtech",
+    fuel: 
+  },
+};
